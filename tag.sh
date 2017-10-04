f554d66c6bedfde089ee88b5344df50efde8759a
bbounvic <bbounvic@gmail.com>
2017-10-04 15:52:09 -0400

    Fixing copy command, old command was not running on Mac terminal correctly

--- tag.sh
+++ tag.sh
@@ -9,7 +9,8 @@ echo "Run with tag $VERSION"
 
 git checkout gh-pages
 git rm -rf CodeSystem sheets
-cp -r dist/CodeSystem/ dist/sheets/ .
+cp -r dist/CodeSystem/ CodeSystem
+cp -r dist/sheets/ sheets
 git add CodeSystem/ sheets/
 git commit -m "Publish $VERSION"
 git tag $VERSION
