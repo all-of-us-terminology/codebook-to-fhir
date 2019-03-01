78113fc7457e05f3aeb735b3e5cfca0827211d38
bobby <bbounvic@gmail.com>
2019-02-28 23:24:45 -0500

    adding make script for windows users

---
+++ make.sh
@@ -0,0 +1,23 @@
+#script for windows users without Make execution
+if [ -z $1 ]
+then
+  command="invalid"
+elif [ -n $1 ]
+then
+# otherwise make first arg as the command
+  command=$1
+fi
+
+# use case statement to make decision for which command.
+case $command in
+   "build") echo "runnig build"
+             rm -rf dist/* && python build.py --config config/ppi-codebook.json;;
+   "validate-prerelease") echo "running validate-prerelease"
+              rm -rf dist/* && python build.py --config config/ppi-codebook-prerelease.json;;
+   "tag") echo "Running build and tag"
+              rm -rf dist/* && python build.py --config config/ppi-codebook.json && tag.sh;;
+   "publish") echo "Running publish"
+              rm -rf dist/* && python build.py --config config/ppi-codebook.json && tag.sh && git push origin gh-pages && git push --tags;;
+   "invalid") echo "Error: Please input a valid command [build | validate-prerelease | tag | publish]";;
+   *) echo "Error: There is no command  for $command.";;
+esac
